c0dbc8ea5ed018183fe8b9f0a8bd137e21893fca
Ruhul <ruhulinfo50@gmail.com>
2023-01-07 11:57:01 +0600

    feat: Add blog section

---
+++ components/card/FoodCard.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+
+function FoodCard() {
+  return (
+    <div className="shadow rounded-[10px] text-[12px]">
+      <div className="">
+        <Image
+          src="/assets/images/pizza.svg"
+          alt="pizza"
+          width={277}
+          height={250}
+        />
+      </div>
+      <div className="flex items-center justify-between my-[1rem] px-[12.5px]">
+        <p>Home made pizza</p>
+        <p>$19</p>
+      </div>
+
+      <div className="flex items-center justify-between px-[12.5px] pb-[12.5px]">
+        <div className="flex items-center space-x-[12px]">
+          <div className="flex items-center bg-[#F7F8FA] py-[4px] px-[6px] space-x-[6px] rounded-[5px]">
+            <Image
+              src="/assets/icons/star.svg"
+              alt="star"
+              width={12}
+              height={12}
+            />
+            <span>4.7</span>
+          </div>
+          <div className="flex items-center bg-[#F7F8FA] p-[4px] space-x-[6px] rounded-[5px]">
+            <span>50-79 min</span>
+          </div>
+        </div>
+        <div>
+          <button className="bg-[#F3BA00] p-[.2rem]">
+            <Image
+              src="/assets/icons/btn-icon.svg"
+              alt="btn-icon"
+              width={11}
+              height={11}
+            />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FoodCard;
